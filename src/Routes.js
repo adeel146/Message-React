e213a9f6daed88d311230c0a6535ee148c2c2c8f
adeel146 <adeelmuhammad146@gmail.com>
2022-01-14 20:12:04 +0500

    lll

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TodoAppBar from "./Components/Appbar";
 import BasicTable from "./Components/Messages";
-import ViewOneItem from "./Components/ViewOneItem";
 import SignIn from "./Components/SignIn";
 import RequireAuth from "./Components/RequiredAuth";
 import NotFoundPage from "./Components/NotFoundPage";
@@ -15,7 +14,6 @@ function Configuration() {
         <Route element={<RequireAuth/>}>
           <Route path="home" element={<BasicTable />} />
         </Route>
-        <Route path="view:name" element={<ViewOneItem />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
